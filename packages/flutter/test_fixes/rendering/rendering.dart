cbbf2b4e759c6ae79423f9c4bd7c8c2ad22fa9e7
fischerscode <github@maaeps.de>
2022-11-22 16:21:42 +0100

    add fix_data

--- packages/flutter/test_fixes/rendering/rendering.dart
+++ packages/flutter/test_fixes/rendering/rendering.dart
@@ -5,6 +5,13 @@
 import 'package:flutter/rendering.dart';
 
 void main() {
+  // TODO: Add the pull request id
+  // Changes made in https://github.com/flutter/flutter/pull/
+  CustomPainter painter;
+  painter.hitTest(position);
+  _CustomPainter painter2;
+  painter2.hitTest(position);
+
   // Changes made in https://github.com/flutter/flutter/pull/66305
   RenderStack renderStack = RenderStack();
   renderStack = RenderStack(overflow: Overflow.visible);
@@ -18,4 +25,11 @@ void main() {
   renderListWheelViewport = RenderListWheelViewport(clipToSize: false);
   renderListWheelViewport = RenderListWheelViewport(error: '');
   renderListWheelViewport.clipToSize;
+}
+
+// TODO: Add the pull request id
+// Changes made in https://github.com/flutter/flutter/pull/
+class _CustomPainter extends CustomPainter {
+  @override
+  bool? hitTest(Offset position) => super.hitTest(position);
 }
